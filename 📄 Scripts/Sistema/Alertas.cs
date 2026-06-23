bb4753f161766c4ed6468cc3125d5ab9a0d5e90f
Alessandro Eustaquio <pixsandro77@gmail.com>
2026-06-23 09:31:25 -0300

    Create Alertas.cs

---
+++ 📄 Scripts/Sistema/Alertas.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public static class Alertas
+{
+    public static void MostrarAlerta(string mensagem)
+    {
+        Debug.Log("📢 " + mensagem);
+        // Aqui depois você liga para aparecer na tela do jogo
+    }
+}
